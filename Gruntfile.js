1e69c1632be9a80535018018a011e63b86cc0489
Robert Velickovski <robert.velickovski@q.agency>
2025-06-19 16:34:07 +0200

    - drop bower support - add npmignore -  add and define browserslistrc - add license - add editorconfig - update outdated dev packages - define default dev and build npm scripts - apply prettier code  formatting - demo update - bump into v1.0.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,75 +1,74 @@
-module.exports = function(grunt) {
+const sass = require("sass");
+const autoprefixer = require("autoprefixer");
 
-    grunt.initConfig({
+module.exports = function (grunt) {
+	grunt.initConfig({
+		settings: {
+			srcPath: "src/",
+			distPath: "dist/",
+		},
 
-        settings: {
-            srcPath: 'src/',
-            distPath: 'dist/'
-        },
+		sass: {
+			options: {
+				implementation: sass, // Add this line
+			},
+			app: {
+				files: [
+					{
+						expand: true,
+						cwd: "<%= settings.srcPath %>",
+						src: ["**/*.scss"],
+						dest: "<%= settings.distPath %>",
+						ext: ".css",
+					},
+				],
+				options: {
+					outputStyle: "compressed",
+					sourceMap: false,
+					precision: 5,
+				},
+			},
+		},
 
-        sass: {
-            app: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= settings.srcPath %>',
-                    src: ['**/*.scss'],
-                    dest: '<%= settings.distPath %>',
-                    ext: '.css'
-                }],
-                options: {
-                    outputStyle: 'compressed',
-                    sourceMap: false,
-                    precision: 5
-                }
-            }
-        },
+		postcss: {
+			options: {
+				processors: [autoprefixer()],
+			},
+			dist: {
+				src: "<%= settings.distPath %>*.css",
+			},
+		},
 
-        postcss: {
-            options: {
-                map: false,
-                processors: [
-                    require('autoprefixer')({
-                        browsers: 'last 10 versions, Explorer >= 9'
-                    })
-                ]
-            },
-            dist: {
-                src: '<%= settings.distPath %>*.css'
-            }
-        },
+		htmlbuild: {
+			build: {
+				expand: true,
+				cwd: "<%= settings.srcPath %>",
+				src: "**/*.html",
+				dest: "<%= settings.distPath %>",
+			},
+		},
 
-        htmlbuild: {
-            build: {
-                expand: true,
-                cwd: '<%= settings.srcPath %>',
-                src: '**/*.html',
-                dest: '<%= settings.distPath %>'
-            }
-        },
+		watch: {
+			scss: {
+				expand: true,
+				files: ["<%= settings.srcPath %>sass/**/*.scss"],
+				tasks: ["sass", "postcss"],
+				options: {
+					spawn: false,
+				},
+			},
+			html: {
+				files: ["<%= settings.srcPath %>*.html"],
+				tasks: ["htmlbuild"],
+				options: {
+					spawn: false,
+				},
+			},
+		},
+	});
 
-        watch: {
-            scss: {
-                expand: true,
-                files: ['<%= settings.srcPath %>sass/**/*.scss'],
-                tasks: ['sass', 'postcss'],
-                options: {
-                    spawn: false
-                }
-            },
-            html: {
-                files: ['<%= settings.srcPath %>*.html'],
-                tasks: ['htmlbuild'],
-                options: {
-                    spawn: false
-                }
-            }
-        }
+	require("load-grunt-tasks")(grunt);
 
-    });
-
-    require('load-grunt-tasks')(grunt);
-
-    grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['sass', 'postcss', 'htmlbuild']);
-
+	grunt.registerTask("default", ["watch"]);
+	grunt.registerTask("build", ["sass", "postcss", "htmlbuild"]);
 };
